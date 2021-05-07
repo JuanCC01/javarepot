bd6c9e68c45b0f7112dd754f3f5b7fc90aca9765
user <user@PC-Juan>
2021-05-07 14:06:23 +0200

    cambiado

--- Hola/src/Hola.java
+++ Hola/src/Hola.java
@@ -2,7 +2,8 @@
 public class Hola {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+		System.out.println("Fin de la clase");
 
 	}
 
